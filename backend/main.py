8da11d710000fa53df162a5d1d8b6c1c897717ec
pandurajup <p.panduraju@gmail.com>
2026-03-25 23:17:11 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -3,8 +3,11 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from backend.services.data_service import get_stock_data
 from backend.services.feature_service import get_features
-from backend.services.prediction_service import predict_signal, get_top_picks
-from backend.services.prediction_service import get_portfolio
+from backend.services.prediction_service import (
+    predict_signal,
+    get_top_picks,
+    get_portfolio
+)
 
 # 🚀 Initialize app
 app = FastAPI()
@@ -47,10 +50,12 @@ def features(symbol: str):
 def predict(symbol: str):
     return predict_signal(symbol)
 
-# ⭐ NEW — Top AI Picks
+# ⭐ Top AI Picks
 @app.get("/top-picks")
 def top_picks():
     return get_top_picks()
+
+# 💰 Portfolio AI
 @app.get("/portfolio")
 def portfolio():
     return get_portfolio()
