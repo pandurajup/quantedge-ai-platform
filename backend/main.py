946c97231eb8b7751d01f5f3a0d47ab11c952454
pandurajup <p.panduraju@gmail.com>
2026-03-25 15:03:36 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from backend.services.data_service import get_stock_data
-
+from backend.services.feature_service import get_features
 app = FastAPI()
 
 @app.get("/")
@@ -18,3 +18,6 @@ def health():
 @app.get("/stock/{symbol}")
 def stock(symbol: str):
     return get_stock_data(symbol)
+@app.get("/features/{symbol}")
+def features(symbol: str):
+    return get_features(symbol)
